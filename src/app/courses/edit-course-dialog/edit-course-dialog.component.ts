301965fe434109d72c546a3d0920df44f19a33da
Ross Acheson <racheson@fivestardev.com>
2021-05-21 17:49:59 -0400

    use CourseEntityService to optimistically update a course

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -3,7 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Course } from '../model/course';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { CoursesHttpService } from '../services/courses-http.service';
+
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'course-dialog',
@@ -26,7 +27,7 @@ export class EditCourseDialogComponent {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<EditCourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) data,
-        private coursesService: CoursesHttpService) {
+        private coursesService: CourseEntityService) {
 
         this.dialogTitle = data.dialogTitle;
         this.course = data.course;
@@ -62,11 +63,10 @@ export class EditCourseDialogComponent {
             ...this.form.value
         };
 
-        this.coursesService.saveCourse(course.id, course)
-            .subscribe(
-                () => this.dialogRef.close()
-            );
-
+        if (this.mode === 'update') {
+            this.coursesService.update(course);
+            this.dialogRef.close();
+        }
     }
 
 
