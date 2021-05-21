b78171d1bdb139418a10fa0fda207574628649b1
Ross Acheson <racheson@fivestardev.com>
2021-05-21 16:25:47 -0400

    Refactor HomeComponent to use CourseEntityService

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -5,7 +5,7 @@ import { defaultDialogConfig } from '../shared/default-dialog-config';
 import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { map, shareReplay } from 'rxjs/operators';
-import { CoursesHttpService } from '../services/courses-http.service';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'home',
@@ -15,15 +15,13 @@ import { CoursesHttpService } from '../services/courses-http.service';
 export class HomeComponent implements OnInit {
 
     promoTotal$: Observable<number>;
-
-    loading$: Observable<boolean>;
-
     beginnerCourses$: Observable<Course[]>;
     advancedCourses$: Observable<Course[]>;
 
     constructor(
         private dialog: MatDialog,
-        private coursesHttpService: CoursesHttpService) {
+        private coursesService: CourseEntityService
+    ) {
 
     }
 
@@ -32,31 +30,20 @@ export class HomeComponent implements OnInit {
     }
 
     reload() {
-
-        const courses$ = this.coursesHttpService.findAllCourses()
-            .pipe(
-                map(courses => courses.sort(compareCourses)),
-                shareReplay()
-            );
-
-        this.loading$ = courses$.pipe(map(courses => !!courses));
-
-        this.beginnerCourses$ = courses$
+        this.beginnerCourses$ = this.coursesService.entities$
             .pipe(
                 map(courses => courses.filter(course => course.category === 'BEGINNER'))
             );
 
-
-        this.advancedCourses$ = courses$
+        this.advancedCourses$ = this.coursesService.entities$
             .pipe(
                 map(courses => courses.filter(course => course.category === 'ADVANCED'))
             );
 
-        this.promoTotal$ = courses$
+        this.promoTotal$ = this.coursesService.entities$
             .pipe(
                 map(courses => courses.filter(course => course.promo).length)
             );
-
     }
 
     onAddCourse() {
