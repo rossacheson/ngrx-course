d977b69242cc6fd8e16bff0f61c8309f5daa9b12
Ross Acheson <racheson@fivestardev.com>
2021-06-04 14:24:40 -0400

    Formatting

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,49 +1,49 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom } from 'rxjs/operators';
+import { CoursesHttpService } from '../services/courses-http.service';
 
 
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
+    course$: Observable<Course>;
 
-  lessons$: Observable<Lesson[]>;
+    lessons$: Observable<Lesson[]>;
 
-  displayedColumns = ['seqNo', 'description', 'duration'];
+    displayedColumns = ['seqNo', 'description', 'duration'];
 
-  nextPage = 0;
+    nextPage = 0;
 
-  constructor(
-    private coursesService: CoursesHttpService,
-    private route: ActivatedRoute) {
+    constructor(
+        private coursesService: CoursesHttpService,
+        private route: ActivatedRoute) {
 
-  }
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
+        const courseUrl = this.route.snapshot.paramMap.get('courseUrl');
 
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
+        this.course$ = this.coursesService.findCourseByUrl(courseUrl);
 
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
+        this.lessons$ = this.course$.pipe(
+            concatMap(course => this.coursesService.findLessons(course.id)),
+            tap(console.log)
+        );
 
-  }
+    }
 
 
-  loadLessonsPage(course: Course) {
+    loadLessonsPage(course: Course) {
 
-  }
+    }
 
 }
